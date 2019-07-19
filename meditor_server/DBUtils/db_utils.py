b8887c017c54e7855b2d528c59072bdcde10f8f4
Roche Periyanayagam <rocheinside@gmail.com>
2019-07-19 08:15:58 +0530

    db queries addition

--- meditor_server/DBUtils/db_utils.py
+++ meditor_server/DBUtils/db_utils.py
@@ -13,6 +13,17 @@ class DB_UTILS:
 
         self.conn = psycopg2.connect(host=hostname, port=port, database=databasename, user=username, password=password)
 
+    #Default for insert queries
+    def execute_query(self, query):
+        print("Going to execute query --- " + query)
+        cur = self.conn.cursor()
+        # cur.execute('select * from queen')
+        cur.execute(query)
+        # db_users = cur.fetchone()
+        # print(db_users)
+        self.conn.commit()
+        cur.close()
+
     def get_data_from_database(self, query):
         pass
 
@@ -26,12 +37,27 @@ class DB_UTILS:
 
     def add_patient_details(self, first_name, last_name, age, gender, email):
         query = "insert into patientdetails(firstname, lastname, age, gender, email) values('"+first_name+"','"+last_name+"','"+age+"','"+gender+"','"+email+"')"
-        print(query);
-        cur = self.conn.cursor()
-        # cur.execute('select * from queen')
-        cur.execute(query)
-        # db_users = cur.fetchone()
-        # print(db_users)
-        self.conn.commit()
-        cur.close()
+        self.execute_query(query)
 
+
+    def add_pill_details(self, pill_name):
+        query = "insert into tabletdetails(tabletname) values('" + pill_name + "')"
+        self.execute_query(query)
+
+    def add_bed_details(self, bed_number):
+        query = "insert into beddetails(bednumber) values('" + bed_number + "')"
+        self.execute_query(query)
+
+    def change_patient_details(self, first_name, last_name, age, gender, email, patient_id):
+        query = "update patientdetails set " \
+                "firstname='"+first_name+"', " \
+                "lastname='"+last_name+"', " \
+                "age='"+age+"', " \
+                "gender='"+gender+"', " \
+                "email='"+email+"' " \
+                "where patientid=" + patient_id
+        self.execute_query(query)
+
+    def change_pill_details(self, pill_name, pill_id):
+        query = "update tabletdetails set tablename='" + pill_name +"' where tabletid=" + pill_id
+        self.execute_query(query)
