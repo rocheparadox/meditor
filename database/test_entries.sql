7733779e2447f59c02aefdbaaf20507f14f87d63
rocheparadox <rocheinside@gmail.com>
2019-09-15 22:24:14 +0530

    Test entries for db --- initial commit

---
+++ database/test_entries.sql
@@ -0,0 +1,19 @@
+\c meditor
+
+/* entries for slots */
+
+INSERT INTO slotdetails VALUES (1, 1, 'open');
+INSERT INTO slotdetails VALUES (2, 2, 'open');
+INSERT INTO slotdetails VALUES (3, 3, 'open');
+INSERT INTO slotdetails VALUES (4, 4, 'open');
+INSERT INTO slotdetails VALUES (5, 5, 'open');
+INSERT INTO slotdetails VALUES (6, 6, 'open');
+INSERT INTO slotdetails VALUES (7, 7, 'open');
+INSERT INTO slotdetails VALUES (8, 8, 'open');
+INSERT INTO slotdetails VALUES (9, 9, 'open');
+INSERT INTO slotdetails VALUES (10, 10, 'open');
+INSERT INTO slotdetails VALUES (11, 11, 'open');
+INSERT INTO slotdetails VALUES (12, 12, 'open');
+INSERT INTO slotdetails VALUES (13, 13, 'open');
+INSERT INTO slotdetails VALUES (14, 14, 'open');
+INSERT INTO slotdetails VALUES (15, 15, 'open');
